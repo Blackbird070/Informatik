324ce1136b2b9b89ff9afe9fc86cf63e56788751
Unknown <finn.eggers@gmx.de>
2018-09-04 21:45:22 +0200

    123

--- pa_codierer/Analyser.java
+++ pa_codierer/Analyser.java
@@ -1,4 +1,37 @@
 package pa_codierer;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class Analyser {
+
+    public static HashMap<Short, Integer> occurencies(short[] ar, int start, int step) {
+        HashMap<Short, Integer> hashMap = new HashMap<>();
+
+        Set<Short> keys = hashMap.keySet();
+
+        for(int i = start; i < ar.length; i+= step){
+            short c = ar[i];
+            if (keys.contains(new Short(c))) {
+                hashMap.put(c, hashMap.get(new Short(c)) + 1);
+            }else{
+                hashMap.put(c, 1);
+            }
+        }
+
+        return hashMap;
+    }
+
+    public static void printHashMap(HashMap<Short, Integer> map) {
+        for (Object a : map.keySet()) {
+            System.out.println(a + "   " + map.get(a));
+        }
+    }
+
+
+    public static void main(String[] args){
+        String s = "HAKJWEOWPDAWZXLEPXHWKBWPDAWLNKREJZAWKBWHAKJWHKZXPA WEJWPDAWJKNPDSAOPWKBWOLXEJWEPOWZEPUWLKLQHXPEKJWKBWWIXGAOWEPWPDAWHXNCAOPWIQJEZELXHEPUWEJWPDAWLNKREJZAWXZZKQJPEJCWBKNWIKNAWPDXJWKJAWMQXNPANWKBWPDAWLNKREJZAOWLKLQHXPEKJWEJZHQ EJCWPDAWIAPNKLKHEPXJWXNAXWPDAWLKLQHXPEKJWEOWAOPEIXPA WXPW";
+        short[] ar = Finn_codierer_3.stringToShort(s);
+        printHashMap(occurencies(ar, 0, 1));
+    }
 }
--- pa_codierer/Finn_codierer_3.java
+++ pa_codierer/Finn_codierer_3.java
@@ -5,95 +5,54 @@ import java.util.Arrays;
 
 public class Finn_codierer_3 {
 
-    
 
-
-    public static char[] stringToChar(String x) {
-        String s = x.toUpperCase();
-
-        char[] a = new char[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            a[i] = s.charAt(i);
-        }
-        return a;
-    }
-
-    public static short[] convert(char[] chars) {
-        short[] a = new short[chars.length];
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == ' ') {
-                a[i] = 0;
+    public static short[] stringToShort(String x) {
+        x = x.toUpperCase();
+        short[] ar = new short[x.length()];
+        for (int i = 0; i < ar.length; i++) {
+            if (x.charAt(i) == ' ') {
+                ar[i] = 0;
             } else {
-                a[i] = (short) (chars[i] - 64);
+                ar[i] = (short) (x.charAt(i) - 64);
             }
         }
-        return a;
+        return ar;
     }
 
-
-<<<<<<< Updated upstream
-    public static short[] keyEncode(short[] s, int key){
-        short[] finish = new short[s.length];
-        for(int i = 0; i < s.length; i++){
-            finish[i] = (short)((s[i] + key) % 27);
-        }
-        return finish;
-    }
-
-    public static String backToBack(short[] a){
+    public static String shortToString(short[] ar) {
         String s = "";
-        for(short sh:a){
-            if(sh == 0) s += " ";
-            else{
-                s += (char)(64 + sh);
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i] == 0) s += ' ';
+            else {
+                s += (char) (ar[i] + 64);
             }
-=======
-    public static void funktion(short[] x) {
-        int i;
-
-        short[] finish = new short[x.length];
-
-
-        for (i = 0; i < x.length; i += 3) {
-            finish[i] = x[(i + 3) % x.length];
-        }
-        for (i = 1; i < x.length; i += 3) {
-            finish[i] = x[(i + 3) % x.length];
-        }
-        for (i = 2; i < x.length; i += 3) {
-            finish[i] = x[(i + 3) % x.length];
->>>>>>> Stashed changes
         }
         return s;
     }
 
-
-        for(i = 0; i < x.length; i++){
-            x[i] = finish[i];
+    public static short[] keyEncode(short[] s, int key) {
+        short[] finish = new short[s.length];
+        for (int i = 0; i < s.length; i++) {
+            finish[i] = (short) ((s[i] + key) % 27);
         }
+        return finish;
+    }
+
+
 
 
     public static void main(String[] arg) {
-        short[] x = convert(stringToChar("leon is the capital of the province of leon located in the northwest of spain Its city population of  makes it the largest municipality in the province accounting for more than one quarter of the provinces population Including the metropolitan area the population is estimated at "));
+        short[] x = stringToShort("leon is the capital of the province of leon located in the northwest of spain Its city population of  makes it the largest municipality in the province accounting for more than one quarter of the provinces population Including the metropolitan area the population is estimated at ");
         System.out.println(Arrays.toString(x));
 
         System.out.println("\n----------------------------------");
-        short[] encode = keyEncode(x , 23);
+        short[] encode = keyEncode(x, 23);
 
-<<<<<<< Updated upstream
         System.out.println(Arrays.toString(encode));
-        System.out.println(backToBack(encode));
-=======
-        funktion(x);
-
-        for (int i = 0; i < x.length; i++) {
-            System.out.print(x[i] + " ");
-        }
+        System.out.println(shortToString(encode));
 
 
 
-
->>>>>>> Stashed changes
     }
 
 
